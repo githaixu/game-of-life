bab56e481f016c6b91d6cd58f9860a40d18eac83
jinhaixu <engkim0826@hotmail.com>
2020-01-08 17:44:49 +0900

    Updates

---
+++ precache-manifest.25b8d11bc0c845134619552b7072b927.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496dd5bcd111608698444cc6bece52f7",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "b378a7a167a1000ada79",
+    "url": "/game-of-life/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b180c957c6be2d95dad9",
+    "url": "/game-of-life/static/css/main.09de4ff3.chunk.css"
+  },
+  {
+    "revision": "b378a7a167a1000ada79",
+    "url": "/game-of-life/static/js/2.85c2544f.chunk.js"
+  },
+  {
+    "revision": "b15a737ad2cfd16685fd27e8e85315d2",
+    "url": "/game-of-life/static/js/2.85c2544f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b180c957c6be2d95dad9",
+    "url": "/game-of-life/static/js/main.ecc95841.chunk.js"
+  },
+  {
+    "revision": "36b8f185d447172bd504",
+    "url": "/game-of-life/static/js/runtime-main.469d5839.js"
+  }
+]);
